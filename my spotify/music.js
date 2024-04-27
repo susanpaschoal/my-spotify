a634c3329b573c73a773d44d034443d5749e4557
susanpaschoal <165685628+susanpaschoal@users.noreply.github.com>
2024-04-26 21:45:35 -0300

    Add files via upload

--- my spotify/music.js
+++ my spotify/music.js
@@ -1,112 +1,177 @@
-const musica = document.getElementById ( "nome-som");       
+const musica = document.getElementById ( "nome-som");
 const banda = document.getElementById ("nome-da-banda")
 const som = document.getElementById("audio")
 const capa1 = document.getElementById("capa1")
 const play = document.getElementById("play")
 const avançar = document.getElementById("avançar")
 const voltar = document.getElementById("voltar")
+const progresso = document.getElementById("progresso")
+const progressoContainer = document.getElementById("progresso-container")
+const aleatoriobutton = document.getElementById("aleatorio")
+
+ 
 
 const pericles = {
+
     musica: "Ate que durou",
     file : 'pericles',
-    banda: "Periclés"
-
+    banda: "Periclés",
 };
+
+ 
 
 const jao = {
     musica: "Acontece",
     file : 'jao',
     banda: "Jão"
-   
 };
+
+ 
 
 const charlie = {
     musica: "Tudo que ela gosta de escutar",
     file : 'charlie',
     banda: "Charlie Brown Jr"
-   
 };
 
 const ferrugem = {
     musica: "Ela",
-    file : 'ferrugem',
+    capa1 : 'ferrugem',
     banda: "Sorriso Maroto,Ferrugem"
-    
+
 };
 
+
 let isPlaying = false;
-const playlist = [pericles, jao, charlie, ferrugem];
+let isShuffled = false;
+const originalPlaylist = [pericles, jao, charlie, ferrugem];
+let sortedPlaylist = [...originalPlaylist];
 let index = 0;
 
 function pauseMusic() {
 
+    isPlaying = false;
     play.querySelector('i.bi').classList.add('bi-play-circle-fill');
     play.querySelector('i.bi').classList.remove('bi-pause-circle-fill');
     som.pause();
-   
 }
 
 function playMusic() {
-
+    isPlaying = true;
     play.querySelector('i.bi').classList.remove('bi-play-circle-fill');
     play.querySelector('i.bi').classList.add('bi-pause-circle-fill');
     som.play();
-    
+
 
 }
-
 
 function playPauseDecider (){
-
-    if(isPlaying === true){
-        pauseMusic();
-
+    if(isPlaying === false){
+        playMusic();
     }
+
     else {
-        playMusic()
+        pauseMusic()
     }
-        
 
-    
+ 
 
 }
+
+ 
 
 function inicioSom(){
-    musica.innerText = playlist[index].musica;
-    banda.innerText = playlist[index].banda;
-    som.src = `/audio/${playlist[index].file}.mp3`;
-    capa1.src = `/imagens/${playlist[index].file}.webp`;
-    
+    musica.innerText = sortedPlaylist[index].musica;
+    banda.innerText = sortedPlaylist[index].banda;
+    som.src = `/audio/${sortedPlaylist[index].file}.mp3`;
+    capa1.src = `/imagens/${sortedPlaylist[index].capa1}.imagem`;
+
+   
+
 }
 
-function voltarMusica(){
-    if(index === 0){
-     index = playlist.length - 1;
+ 
 
+function voltarMusica(){
+
+    if(index === 0){
+     index = sortedPlaylist.length - 1;
+ 
     }
     else{
         index = index - 1;
     }
    inicioSom ();
    playMusic();
-
 }
 
 function avançarMusica(){
-    if(index === playlist.length - 1){
-     index = 0;
 
+    if(index === sortedPlaylist.length - 1){
+     index = 0;
     }
+
     else{
         index = index + 1;
     }
+
    inicioSom ();
    playMusic();
 
 }
 
+
+function atualizarProgresso(){
+   const barralargura = (som.currentTime/som.duration)*100;
+   progresso.style.setProperty('--progress' ,`${barralargura}%`);
+
+}
+
+
+function jumpTo(){
+   const whidth=  progressoContainer.clientWidth;
+   const clickPosition = event.offsetX;;
+   const jumpToTime = (clickPosition/whidth)* som.duration;
+   som.currentTime = jumpToTime;
+
+}
+
+function shuffleArray(preShuffleArray){
+    const size = preShuffleArray.length;
+    let currentIndex = size - 1;
+
+    while(currentIndex > 0){
+
+     let randomIndex =Math.floor(Math.random() * size);
+     let aux = preShuffleArray [ currentIndex];
+     preShuffleArray[currentIndex] = preShuffleArray[randomIndex];
+     preShuffleArray[randomIndex] = aux;
+     currentIndex -= 1;
+
+    }
+
+}
+
+function modoaleatorio(){
+
+    if(isShuffled === false){
+        isShuffled = true;
+        shuffleArray(sortedPlaylist);
+        shuffleButton.classList.add('button-alatorio')    
+
+    } else {
+        isShuffled = false;
+        sortedPlaylist = [...originalPlaylist];
+        shuffleButton.classList.remove('button-alatorio');
+      }
+
+}
+
 inicioSom();
 
-play.addEventListener('click', playMusic);
+play.addEventListener('click', playPauseDecider);
 voltar.addEventListener('click',voltarMusica);
-avançar.addEventListener('click',avançarMusica)
+avançar.addEventListener('click',avançarMusica);
+som.addEventListener('timeupdate',atualizarProgresso);
+progressoContainer.addEventListener('click',jumpTo);
+aleatoriobutton.addEventListener('click', modoaleatorio);
